Alyetama/internetarchive-youtube

commit 4909d90dab701b4bcd199995931c4b21ccaaf68b

Let youtube-dl select the best format

internetarchive_youtube/archive_youtube.py
@@ -265,7 +265,6 @@ class ArchiveYouTube:
                     continue
 
             ydl_opts = {
-                'format': 'mp4/bestaudio+bestvideo',
                 'outtmpl': fname,
                 'quiet': True,
                 'no-warnings': True,

commit f8765627ecbc0a889c0468f8e95277a873d8070c

ArchiveYouTube: Let yt-dlp choose the best format
Fixes: 4909d90dab70 ("Let youtube-dl select the best format")

internetarchive_youtube/archive_youtube.py
@@ -103,7 +103,7 @@ class ArchiveYouTube:
 
     @staticmethod
     def get_video_extension(video_url):
-        with yt_dlp.YoutubeDL({'quiet': True, 'format': 'best'}) as ydl:
+        with yt_dlp.YoutubeDL({'quiet': True}) as ydl:
             try:
                 info = ydl.extract_info(video_url, download=False)
             except Exception as e:
@@ -373,7 +373,7 @@ class ArchiveYouTube:
                 logger.debug(f'Skipped {video} (skip list)...')
                 return
 
-        ydl_opts = {'outtmpl': fname, 'format': 'best'}
+        ydl_opts = {'outtmpl': fname}
 
         if self.no_logs:
             ydl_opts.update({